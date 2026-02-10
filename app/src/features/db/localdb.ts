10179cfdaa80fc2c6bf844123dc1b76021c8dbc5
Aaron Beall <contact@abeall.com>
2026-02-10 06:01:43 +0000

    fix: update database name to production for consistency

--- app/src/features/db/localdb.ts
+++ app/src/features/db/localdb.ts
@@ -136,7 +136,7 @@ export interface LocalDB {
   images: ImageAttachment[];
 }
 
-const DB_NAME = 'number-calendar_test';
+const DB_NAME = 'number-calendar';
 const STORE_DEFS = [
   { name: 'datasets', keyPath: 'id', indexes: [] },
   { name: 'entries', keyPath: ['datasetId', 'date'], indexes: ['datasetId', 'date'] },
